a295a241f99a4954a51e66c5d96676b4e38babaa
Carol Moscheta <carolina.moscheta@gmail.com>
2020-04-02 17:17:50 -0300

    implementação do throw new TypeError e typeof no if

--- src/cipher.js
+++ src/cipher.js
@@ -21,11 +21,10 @@ const cipher = {
   
   decode(offset, message) {
     let decodeMessage = ""
-
-    for (let i in message) {
-      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
-        let encodeFormulaUpperCase = ((message.charCodeAt(i) - 90 - offset) % 26) + 90;
-        decodeMessage += String.fromCharCode(encodeFormulaUpperCase)
+    for (let i in mensagem) {
+      if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+        let criptografiaUpperCase = ((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90;
+        decodeMessage += String.fromCharCode(criptografiaUpperCase)
       
       } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
         let encodeFormulaLowerCase = ((message.charCodeAt(i) - 122 - offset) % 26) + 122;
