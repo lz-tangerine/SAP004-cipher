003bed1edf538fe9d1213c97f472fbdad310f348
Carol Moscheta <carolina.moscheta@gmail.com>
2020-04-09 23:42:21 -0300

    introduzi a parte do código para printar a mensagem na tela quando é clicado o botão de encode ou decode. Ajustei o tipo de botão em button no HTML

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-// import cipher from './cipher.js';
+import cipher from './cipher.js';
 
 let user = localStorage.getItem("user")
 document.getElementById("user").innerHTML = `B3M V1ND0 ${user}`
 
-const botaoEncode = document.getElementById(botaoEncode)
-const botaoDecode = document.getElementById(botaoDecode)
-const encode = document.getElementById(encode)
-const decode = document.getElementById(decode)
+const botaoEncode = document.getElementById("botaoEncode")
+const botaoDecode = document.getElementById("botaoDecode")
+
+function encode (){
+    event.preventDefault()
+    const mensagem = document.getElementById("mensagem").value
+    const chave = Number(document.getElementById("offset").value)
+    document.getElementById("mensagemCriptografada").innerHTML = cipher.encode(chave, mensagem)
+}
+
+function decode (){
+    event.preventDefault()
+    const mensagem = document.getElementById("mensagem").value
+    const chave = Number(document.getElementById("offset").value)
+    document.getElementById("mensagemCriptografada").innerHTML = cipher.decode(chave, mensagem)
+}
+
+
+botaoEncode.addEventListener("click", encode)
+botaoDecode.addEventListener("click", decode)
 
 // console.log(cipher);
