d20d6038b109b1e8e7087a9f89b0e4c749a7a42a
Carol Moscheta <carolina.moscheta@gmail.com>
2020-04-22 00:11:16 -0300

    separação da folha de estilo CSS prara cada arquivo HTML, refatoração do código HTML tirando a parte de style e jogando pro arquivo CSS, refatoração do código cipher, diminuindo a repetição do código

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,37 @@
-const cipher = { 
-  encode: function (chave, mensagem) { //chave de criptografia
-    if (typeof mensagem !== "string") {
-      throw new TypeError();
-    }
-    
-    let mensagemCriptografada = ""
-    for (let i in mensagem) {
-      if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
-        let criptografiaUpperCase = ((mensagem.charCodeAt(i) - 65 + chave) % 26) + 65;
-        mensagemCriptografada += String.fromCharCode(criptografiaUpperCase);
-      
-      } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
-        let criptografiaLowerCase = ((mensagem.charCodeAt(i) - 97 + chave) % 26) + 97;
-        mensagemCriptografada += String.fromCharCode(criptografiaLowerCase);
-      
-      } else{
-        mensagemCriptografada += String.fromCharCode(mensagem.charCodeAt(i));
-      }
-    }
-  return mensagemCriptografada;   
-  },
-  
-  
-  decode: function (chave, mensagem) {
-    if (typeof mensagem !== "string") {
-      throw new TypeError();
-    }
-    
-    let decodeMessage = ""
-    for (let i in mensagem) {
-      if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
-        let criptografiaUpperCase = ((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90;
-        decodeMessage += String.fromCharCode(criptografiaUpperCase)
-      
-      } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
-        let criptografiaLowerCase = ((mensagem.charCodeAt(i) - 122 - chave) % 26) + 122;
-        decodeMessage += String.fromCharCode(criptografiaLowerCase);
-      
-      } else{
-        decodeMessage += String.fromCharCode(mensagem.charCodeAt(i))
-      }
-    }
-  return decodeMessage;
+const cipher = {
+  encode,
+  decode
+};
+
+function encodeOrDecode(chave, mensagem, num) {
+  if (typeof mensagem !== "string") {
+    throw new TypeError("message to be a string");
   }
+
+  let mensagemCriptografada = ""
+  for (let letra in mensagem) {
+    let caracter = mensagem.charCodeAt(letra)
+    if (caracter >= 65 && caracter <= 90) {
+      let criptografiaUpperCase = ((caracter - (65 + num) + chave) % 26) + 65 + num;
+      mensagemCriptografada += String.fromCharCode(criptografiaUpperCase);
+
+    } else if (caracter >= 97 && caracter <= 122) {
+      let criptografiaLowerCase = ((caracter - (97 + num) + chave) % 26) + 97 + num;
+      mensagemCriptografada += String.fromCharCode(criptografiaLowerCase);
+
+    } else {
+      mensagemCriptografada += String.fromCharCode(caracter);
+    }
+  }
+  return mensagemCriptografada;
 }
+
+function encode(chave, mensagem) {
+  return encodeOrDecode(chave, mensagem, 0)
+}
+
+function decode(chave, mensagem) {
+  return encodeOrDecode(-chave, mensagem, 25)
+}
+
 export default cipher
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -42,9 +42,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // it('should return " !@" for " !@"', () => {
-    //   expect(cipher.encode(33, ' !@')).toBe(' !@');
-    // });
+    it('should return " !@" for " !@"', () => {
+      expect(cipher.encode(33, ' !@')).toBe(' !@');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -70,9 +70,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
+    it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+    });
 
     // Hacker edition
 
